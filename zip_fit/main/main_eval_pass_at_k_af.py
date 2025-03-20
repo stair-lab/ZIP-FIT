669c9fb5ad8eb1183a7b6bd42ea41a9781b7c8fd
Brando Miranda <brandojazz@gmail.com>
2025-03-20 16:43:04 -0700

    Enhance README with detailed installation instructions for Lean and Mathlib4, ensuring compatibility with PyPantograph. Update main_eval_pass_at_k_af.py to utilize a new evaluation function and improve logging of model performance metrics. Refactor lean4_comp_pass_at_k.py to include per-batch logging and statistical analysis of pass@k evaluations.

--- zip_fit/main/main_eval_pass_at_k_af.py
+++ zip_fit/main/main_eval_pass_at_k_af.py
@@ -14,6 +14,8 @@ from pantograph import Server
 from utils import seed_everything
 from metrics.lean4_comp_pass_at_k import run_lean4_comp_pass_k_unbiased_eval
 
+from zip_fit.metrics.lean4_comp_pass_at_k import run_lean4_comp_pass_k_unbiased_eval_log_per_completion
+
 def main_eval_lean4_model_performance_pass_at_k(config: dict = {}) -> None:
     """
     Evaluates language model performance on Lean4 theorem statement translation using pass@k methodology.
@@ -30,7 +32,7 @@ conda activate zip_fit; export CUDA_VISIBLE_DEVICES=1; python /lfs/skampere1/0/b
     seed_everything(config.get('seed', 42))
 
     # 0) PyPantograph Lean4 Server
-    server = Server(imports=["Mathlib", "Init"], project_path=os.path.expanduser("~/mathlib4"))
+    server = Server(imports=["Mathlib", "Init"], project_path=os.path.expanduser("~/mathlib4_15_0"))
     # 1) Manual snippet test
     # test_manual_snippets(server)
 
@@ -45,48 +47,34 @@ conda activate zip_fit; export CUDA_VISIBLE_DEVICES=1; python /lfs/skampere1/0/b
     print(f"Currently logged in as: {user_info['name']}\n")
 
     # 3) Model pass@k test (toy)
-    model_name = 'gpt2'
-    model_name = 'UDACA/math-gpt2-zipfit'
-    model_name = 'UDACA/math-gpt2-dsir'
-    model_name = 'UDACA/math-gpt2-less'
-    # model_name = 'internlm/internlm2-math-plus-1_8b'
-    model_name = 'google/gemma-2-2b'
-    model_name = 'UDACA/math-gemma-2-2b-zipfit'
-    model_name = 'UDACA/math-gemma-2-2b-less'
-    model_name = 'UDACA/math-gemma-2-2b-dsir'
+    # model_name = 'gpt2'
+    # model_name = 'UDACA/math-gpt2-zipfit'
+    # model_name = 'UDACA/math-gpt2-dsir'
+    # model_name = 'UDACA/math-gpt2-less'
+    # # model_name = 'internlm/internlm2-math-plus-1_8b'
+    # model_name = 'google/gemma-2-2b'
+    # model_name = 'UDACA/math-gemma-2-2b-zipfit'
+    # model_name = 'UDACA/math-gemma-2-2b-less'
+    # model_name = 'UDACA/math-gemma-2-2b-dsir'
     # model_name = 'mistralai/Mistral-7B-v0.1'
     # model_name = 'meta-llama/Meta-Llama-3-8B'
     # model_name = 'google/gemma-2-2b-it'
+    model_name: str = config.get('model_name', 'gpt2')
     print(f'f{model_name=}') 
+
     # Sanity Check Data
-    # def my_prompt_format(nl_stmt: str) -> str:
-    #     return (
-    #         "Your task is translate the natural language version of the mathematical statement "
-    #         "to a formal Lean statement version, using the following format:\n"
-    #         "natural language statement:\nSuppose that $f$ is holomorphic in an open set $\Omega$. Prove that if $|f|$ is constant, then $f$ is constant.\n"
-    #         "formal Lean language statement:##\ntheorem exercise_1_13c {f : ℂ → ℂ} (Ω : Set ℂ) (a b : Ω) (h : IsOpen Ω) (hf : DifferentiableOn ℂ f Ω) (hc : ∃ (c : ℝ), ∀ z ∈ Ω, abs (f z) = c) : f a = f b:= sorry\n##"
-    #         "natural language statement:\nProve that the power series $\sum zn/n^2$ converges at every point of the unit circle.\n"
-    #         "formal Lean language statement:##\ntheorem exercise_1_19b (z : ℂ) (hz : abs z = 1) (s : ℕ → ℂ) (h : s = (λ n => ∑ i in (range n), i * z / i ^ 2)) : ∃ y, Tendsto s atTop (𝓝 y):= sorry\n##"
-    #         "natural language statement:\nSuppose $f$ is continuous in a region $\Omega$. Prove that any two primitives of $f$ (if they exist) differ by a constant.\n"
-    #         "formal Lean language statement:##\ntheorem exercise_1_26 (f F₁ F₂ : ℂ → ℂ) (Ω : Set ℂ) (h1 : IsOpen Ω) (h2 : IsConnected Ω) (hF₁ : DifferentiableOn ℂ F₁ Ω) (hF₂ : DifferentiableOn ℂ F₂ Ω) (hdF₁ : ∀ x ∈ Ω, deriv F₁ x = f x) (hdF₂ : ∀ x ∈ Ω, deriv F₂ x = f x) : ∃ c : ℂ, ∀ x, F₁ x = F₂ x + c:= sorry\n##"
-    #         f"natural language statement:\n{nl_stmt}\n"
-    #         "formal Lean language statement:"
-    #     )
-    def my_prompt_format(nl_stmt: str) -> str:
-        return (
-            "Your task is translate the natural language version of the mathematical statement "
-            "to a formal Lean statement version, using the following format:\n"
-            "natural language statement:\nLet $z=\frac{1+i}{\sqrt{2}}.$What is $\left(z^{1^2}+z^{2^2}+z^{3^2}+\dots+z^{{12}^2}\right) \cdot \left(\frac{1}{z^{1^2}}+\frac{1}{z^{2^2}}+\frac{1}{z^{3^2}}+\dots+\frac{1}{z^{{12}^2}}\right)?$ $\textbf{(A) } 18 \qquad \textbf{(B) } 72-36\sqrt2 \qquad \textbf{(C) } 36 \qquad \textbf{(D) } 72 \qquad \textbf{(E) } 72+36\sqrt2$ Show that it is \textbf{(C) }36.\n"
-            "formal Lean language statement:##\ntheorem amc12a_2019_p21 (z : ℂ) (h₀ : z = (1 + Complex.I) / Real.sqrt 2) : (∑ k in Finset.Icc 1 12, (z^(k^2))) * (∑ k in Finset.Icc 1 12, (1 / z^(k^2))) = 36 := sorry\n##"
-            "natural language statement:\nIntegers $x$ and $y$ with $x>y>0$ satisfy $x+y+xy=80$. What is $x$? $ \textbf{(A)}\ 8 \qquad\textbf{(B)}\ 10 \qquad\textbf{(C)}\ 15 \qquad\textbf{(D)}\ 18 \qquad\textbf{(E)}\ 26$ Show that it is \textbf{(E)}\ 26.\n"
-            "formal Lean language statement:##\ntheorem amc12a_2015_p10 (x y : ℤ) (h₀ : 0 < y) (h₁ : y < x) (h₂ : x + y + (x * y) = 80) : x = 26 := sorry\n##"
-            "natural language statement:\nWhat is the [[volume]] of a [[cube]] whose [[surface area]] is twice that of a cube with volume 1? $\mathrm{(A)}\ \sqrt{2}\qquad\mathrm{(B)}\ 2\qquad\mathrm{(C)}\ 2\sqrt{2}\qquad\mathrm{(D)}\ 4\qquad\mathrm{(E)}\ 8$ Show that it is \mathrm{(C)}.\n"
-            "formal Lean language statement:##\ntheorem amc12a_2008_p8 (x y : ℝ) (h₀ : 0 < x ∧ 0 < y) (h₁ : y^3 = 1) (h₂ : 6 * x^2 = 2 * (6 * y^2)) : x^3 = 2 * Real.sqrt 2 := sorry\n##"
-            f"natural language statement:\n{nl_stmt}\n"
-            "formal Lean language statement:"
-        )
+    from prompt_templates import prompt_temp_fn_lean4_minif2f_val_v0
+    prompt_fn_name = 'prompt_temp_fn_lean4_minif2f_val_v0'
+    if prompt_fn_name == 'prompt_temp_fn_lean4_minif2f_val_v0':
+        my_prompt_format = prompt_temp_fn_lean4_minif2f_val_v0
+    else:
+        raise ValueError(f'Invalid prompt function name: {prompt_fn_name}')
+
     from datasets import load_dataset
-    ds_test = load_dataset('UDACA/proofnet-v3-lean4', split='test')
+    ds_test = load_dataset(
+        config.get('eval_dataset', 'UDACA/proofnet-v3-lean4'), 
+        split=config.get('split_name', 'test')
+    )
     # ds_test = ds_test.select(list(range(10)))
 
     # Promptify & get Gold Truth Headers
@@ -105,18 +93,19 @@ conda activate zip_fit; export CUDA_VISIBLE_DEVICES=1; python /lfs/skampere1/0/b
     debug = True
     # debug = False
     eval_batch_size = 32 # for vllm how many prompts to batch for speed
-    k = 5
-    # num_samples = 20
-    # num_samples = 40
-    num_samples = 5000
-    score = run_lean4_comp_pass_k_unbiased_eval(prompts, model_name, server, headers=gold_headers, k=k, num_samples=num_samples, eval_batch_size=eval_batch_size, debug=debug)
-    print(f"\n==== For {model_name} Final Average Pass@{k=}N={num_samples} across {len(prompts)} tasks: {score:.3f} ====\n")
+    k = 5 # the k specific the actualy metric for pass@k
+    # num_samples = 20 # the big N in pass@k - total number of completions generated
+    # num_samples = 40 # the big N in pass@k - total number of completions generated
+    num_samples = 5000 # the big N in pass@k - total number of completions generated
+    final_avg_pass_val = run_lean4_comp_pass_k_unbiased_eval_log_per_completion(prompts, model_name, server, headers=gold_headers, k=k, num_samples=num_samples, eval_batch_size=eval_batch_size, debug=debug)
+    print(f"\n==== For {model_name} Final Average Pass@{k=}N={num_samples} across {len(prompts)} tasks: {final_avg_pass_val:.3f} ====\n")
 
     # End overall timer
     global_end_time = time.time()
     total_seconds = global_end_time - global_start_time
     print(f"\nDone. Total run time for all models: {total_seconds:.2f} seconds, {total_seconds/60:.2f} minutes, {total_seconds/3600:.2f} hours.\a")
-
+    wandb.log({"avg pass@k(D,p)": final_avg_pass_val})
+    return final_avg_pass_val
 
 def main_experiment_pass_k_vs_N_config(config: dict = {}):
     """
@@ -379,8 +368,13 @@ def _main(**kwargs):
     run = wandb.init(mode=kwargs.get('mode', 'dryrun'), project="zip-fit-pass-at-k-af", name=run_name, save_code=True, config=kwargs)
     wandb.save(__file__) # save current code now, don't wait to wandb.finish, also useful: wandb.save("*.py") # upload all .py files in current directory
     print(f'Kwargs to run:\n{kwargs}')
-    main_eval_lean4_model_performance_pass_at_k(kwargs)
-    # main_experiment_pass_k_vs_N_config(kwargs)
+    main_fn_name: str = kwargs.get('main_fn_name', 'None')
+    if main_fn_name == 'main_eval_lean4_model_performance_pass_at_k':
+        main_eval_lean4_model_performance_pass_at_k(kwargs)
+    elif main_fn_name == 'main_experiment_pass_k_vs_N_config':
+        main_experiment_pass_k_vs_N_config(kwargs)
+    else:
+        raise ValueError(f'Invalid function name: {kwargs.get("main_fn_name", "None")=}')
     run.alert(title="Run Completed", text=f"Run finished, run url: {run.get_url()}")
     print(f'{run.get_url()=}')
     wandb.finish()
