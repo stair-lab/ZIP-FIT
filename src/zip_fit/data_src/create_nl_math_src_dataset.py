d9f13d5c461f00d2e25c526476545901b43b0aea
Brando Miranda <brandojazz@gmail.com>
2025-05-06 01:59:10 +0000

    Update dataset entries in create_nl_math_src_dataset.py to include new sources and adjust existing dataset configurations for improved data diversity and training effectiveness.

--- src/zip_fit/data_src/create_nl_math_src_dataset.py
+++ src/zip_fit/data_src/create_nl_math_src_dataset.py
@@ -10,26 +10,29 @@
 # Target settings and datasets
 target_settings = [
     # choosing vars as test since it's unlikely models were trained on it
-    ('brando/putnam-axiom-dataset-text-only', 'func_variations_265_10_30_2024', 265, "Putnam Test (Vars)")
+    ('Putnam-AXIOM/putnam-axiom-dataset-ICML-2025', 'test', 350), 
 ]
 
 # List of source datasets in the specified order
 src_datasets = [
     # Goal: around 100k total examples or documents
+
     # 10-25% really good data
-    ('openai/gsm8k', 'train', 7_473),
-    ('brando/hendrycks_math', 'train', 5_500),
-    ('brando/olympiad-bench-imo-math-boxed-825-v2-21-08-2024', 'train', 700),
-    ('brando/putnam-axiom-dataset-text-only', 'full_original_236_10_30_2024', 236), 
+    ('Putnam-AXIOM/putnam-axiom-dataset-ICML-2025', 'dev', 150), 
     ('hoskinson-center/proofnet', 'validation', 185),
+    ('brando/olympiad-bench-imo-math-boxed-825-v2-21-08-2024', 'train', 700),
 
     # 25-50% somewhat related
+    ('brando/hendrycks_math', 'train', 5_500),
     ('TIGER-Lab/MathInstruct', 'train', 3_000), # Mammoth = "COT & POP"
     ('TIGER-Lab/WebInstructSub', 'train', 3_000), # Mammoth 2 = "Problem:...Soln:...pairs from CC made into pairs by LLMs"
     ('brando/small-open-web-math-dataset-v2', 'train', 9_000),
+    ('openai/gsm8k', 'train', 7_473),
 
     # 50% unrelated or really bad data
-    ('brando/small-c4-dataset', 'train', 5_000),
+    ('brando/small-c4-dataset', 'train', 10_000), 
+    ('brando/small-c4-dataset', 'validation', 10_000), 
+    ('brando/small-c4-dataset', 'test', 10_000), 
     ('iamtarun/python_code_instructions_18k_alpaca', 'train', 18_612),
     ('brando/random-all-ascii-dataset', 'train', 5_000)
 
